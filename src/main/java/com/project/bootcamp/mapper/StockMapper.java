dd83b8a162f6bc9c6d6fefacc07ca59696a897fb
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-06-29 12:22:05 -0300

    Interando API com Banco

---
+++ src/main/java/com/project/bootcamp/mapper/StockMapper.java
@@ -0,0 +1,19 @@
+package com.project.bootcamp.mapper;
+
+import com.project.bootcamp.model.Stock;
+import com.project.bootcamp.model.dto.StockDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StockMapper {
+
+    public Stock toEntity(StockDTO dto) {
+        Stock stock = new Stock();
+        stock.setId(dto.getId());
+        stock.setName(dto.getName());
+        stock.setPrice(dto.getPrice());
+        stock.setVariation(dto.getVariation());
+        stock.setDate(dto.getDate());
+        return stock;
+    }
+}
